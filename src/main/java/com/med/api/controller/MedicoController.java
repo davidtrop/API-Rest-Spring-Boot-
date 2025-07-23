99d96087235337c4d9b1c47e4bc65d379ab8fd21
David Silva <david.silva@doctum.edu.br>
2025-07-23 09:34:13 -0300

    Criando validação nos campos vindos do post

--- src/main/java/com/med/api/controller/MedicoController.java
+++ src/main/java/com/med/api/controller/MedicoController.java
@@ -3,7 +3,9 @@ package com.med.api.controller;
 import com.med.api.medico.DadosCadastroMedico;
 import com.med.api.medico.Medico;
 import com.med.api.medico.MedicoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +19,9 @@ public class MedicoController {
     private MedicoRepository repository;
 
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+
         repository.save(new Medico(dados));
     }
 }
