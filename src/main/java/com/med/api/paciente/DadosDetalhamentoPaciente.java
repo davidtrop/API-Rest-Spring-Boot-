acc4368362fc3c0dd99a7756ee2305966bd52af9
David Silva <david.silva@doctum.edu.br>
2025-07-28 10:08:43 -0300

    Adicionando tratamento de retorno com ResponseEntity, adicionando tratamento nas devolucoes de dados, adicionando cabecalho nas resposta da api, e ultilizando codigos http mais apropriados

---
+++ src/main/java/com/med/api/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,10 @@
+package com.med.api.paciente;
+
+import com.med.api.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(Long id, String nome, String email, String telefone, String cpf, Endereco endereco) {
+
+    public DadosDetalhamentoPaciente(Paciente paciente){
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
+    }
+}
