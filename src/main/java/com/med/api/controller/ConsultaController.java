6e50fb0efe21d739184fb9dd2a8821afb8dc700b
David Silva <david.silva@doctum.edu.br>
2025-08-11 11:26:00 -0300

    Adicionando a biblioteca SpringDoc com adaptacao ao JWT

--- src/main/java/com/med/api/controller/ConsultaController.java
+++ src/main/java/com/med/api/controller/ConsultaController.java
@@ -3,7 +3,7 @@ package com.med.api.controller;
 import com.med.api.domain.consulta.AgendaDeConsultas;
 import com.med.api.domain.consulta.DadosAgendamentoConsulta;
 import com.med.api.domain.consulta.DadosCancelamentoConsulta;
-import com.med.api.domain.consulta.DadosDetalhamentoConsulta;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 
     @Autowired
